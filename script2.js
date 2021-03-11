76ed12bef52aea5b81c7de766ff411dcdb8e908f
jayanth.s <jayanthsibbala123@gmail.com>
2021-03-12 00:13:43 +0530

    (jsontohtnl)

--- script2.js
+++ script2.js
@@ -1,93 +1,127 @@
-var Template ={
-  div : {
-      name: "div",
-      class:"animal",
-      content1:{
-        name : "h1",
-         innerText:"how are you",
-         class: "blue"
-      },
-      content2:{
-          name : "img",
-          src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg",
-          class:"image"
-      }
-  },
-  div2 : [
-        {
-      name: "div",
-      class:"animal",
-      content1:{
-        name : "img",
-        src:"https://learnwebcode.github.io/json-example/images/cat-2.jpg",
-        class:"image",
-        innerText:"cat"
-
-          }
-        },
-          {
-      name: "div",
-      class:"animal",
-      content1:{
-        name : "img",
-        src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg",
-        class:"image",
-        innerText:"Dog"
-    }
-  },
+var HtmlModel = {
+content:[
     {
-      name: "div",
-      class:"animal",
-      content1:{
-        name : "img",
-        src:"https://learnwebcode.github.io/json-example/images/cat-1.jpg",
-        class:"image",
-        innerText:"cat"
-    }
+      name:"div",
+      attributes:{id:"",class:"animal",src:""},
+      innerText:"how are you",
+      subcontent: [
+        {name : "img",
+        attributes:{id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg"},
+        innerText:"",
+        }
+        ]
+         },
+     [
+          {
+        name: "div",
+        attributes:{id:"",class:"animal",src:""},
+        innerText:"cat",
+        subcontent:[ 
+          {name : "img",
+         attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/cat-2.jpg"},
+         innerText:"cat"}
+        ]
+            },
+        {
+              name: "div",
+              attributes:{id:"",class:"animal",src:""},
+              innerText:"Dog",
+              subcontent:[ 
+                {name : "img",
+               attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg"},
+               innerText:"cat"}
+              ]
+        },
+           
+      {
+        name: "div",
+              attributes:{id:"",class:"animal",src:""},
+              innerText:"cat",
+              subcontent:[ 
+                {name : "img",
+               attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/cat-1.jpg"},
+               innerText:"cat"}
+              ]
+        }
+      ]
+   ]
   }
 
-  ]  
+function setAttributes(name,attribute) {
+console.log("setattributes is called");
+  console.log(name);
+  for( var key in attribute){
+ name.setAttribute(key,attribute[key]);
+ 
+console.log(key);
+console.log(attribute[key]);
 }
-function converter(template){
-  console.log(key);
-  for (var key in template)
-  { 
-    var value = template[key]; 
-  if(Array.isArray(value))
-  {
-    for (i = 0; i < value.length; i++) {
-     
-    let div = document.createElement(value[i].name);
-    div.classList.add(value[i].class);
-    document.body.appendChild(div);
-    
-    let img = document.createElement(value[i].content1.name);
-    img.classList.add(value[i].content1.class);
-    img.setAttribute("src",value[i].content1.src);
-    div.innerHTML = value[i].content1.innerText;
-    div.appendChild(img);
-    }
-  }  
+  
+}
 
-  else if ( typeof(value)=="object")
-  {
-    console.log(key)
-    let div = document.createElement(value.name);
-    div.classList.add(value.class);
-    let h1 = document.createElement(value.content1.name);
-    h1.classList.add(value.content1.class);
-    h1.innerText = value.content1.innerText;
-    let img = document.createElement(value.content2.name);
-    img.classList.add(value.content2.class);
-    img.setAttribute("src",value.content2.src);
+function elementCreate(value){
+  console.log(value);
+ console.log("create function is called")
+  var parentEntity;
+  var childEntity;
+  parentEntity = document.createElement(value.name);
+  console.log(parentEntity);
+  parentEntity.innerText=value.innerText;
+  setAttributes(parentEntity,value.attributes);
+  if(value.subcontent)
+  
+  {for (i=0;i< value.subcontent.length;i++){
+    if(typeof(value.subcontent[i])=="object")
+    {
+      childEntity = elementCreate(value.subcontent[i]);
+      console.log("there is a subcontent")
+      console.log(childEntity)
+    }
+  }
+}
+  else{
+    return parentEntity;
+  }
+  elementAppend(parentEntity,childEntity);
+  console.log(parentEntity);
+  return parentEntity;
+
+}
+function elementAppend(parent,child) {
+  console.log("append function is called");
+  parent.appendChild(child);
+  console.log(parent);
+  console.log(child);
+  console.log(parent.innerHTML)
+}
+
+function process(HtmlModel){
+
+  for  (var i = 0; i < HtmlModel.content.length; i++)
+  { 
+    var value = HtmlModel.content[i]; 
+    console.log(i);
+    if (Array.isArray(value))
+    {
+      console.log("loop entered")
+      for (var i=0; i<=value.length;i++){
+        entity2 = elementCreate(value[i]);
+        console.log(entity2);
+        document.getElementById("main-body").appendChild(entity2);
+      }
+    }
+  else {
+    entity = elementCreate(value);
+    console.log(entity);
+   document.getElementById("main-body").appendChild(entity);
+    console.log("child appended");
     
-    div.appendChild(img);
-   document.body.appendChild(div);
-    div.appendChild(h1);
+    }
+    
+  
   }
     
 }  
 
-}
-window.onload = converter(Template)  
 
+window.onload = process(HtmlModel) 
