c166f4de1b74ca7d27bb58cc0618947ae44dc612
jayanth.s <jayanthsibbala123@gmail.com>
2021-03-12 13:29:14 +0530

    (html2json)

---
+++ jsons.js
@@ -0,0 +1,48 @@
+ HtmlModel = {
+    content:[
+        {
+          name:"div",
+          attributes:{id:"",class:"animal",src:""},
+          innerText:"how are you",
+          subcontent: [
+            {name : "img",
+            attributes:{id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg"},
+            innerText:"",
+            }
+            ]
+             },
+         [
+              {
+            name: "div",
+            attributes:{id:"",class:"animal",src:""},
+            innerText:"cat",
+            subcontent:[ 
+              {name : "img",
+             attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/cat-2.jpg"},
+             innerText:"cat"}
+            ]
+                },
+            {
+                  name: "div",
+                  attributes:{id:"",class:"animal",src:""},
+                  innerText:"Dog",
+                  subcontent:[ 
+                    {name : "img",
+                   attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/dog-1.jpg"},
+                   innerText:"cat"}
+                  ]
+            },
+               
+          {
+            name: "div",
+                  attributes:{id:"",class:"animal",src:""},
+                  innerText:"cat",
+                  subcontent:[ 
+                    {name : "img",
+                   attributes: {id:"",class:"image",src:"https://learnwebcode.github.io/json-example/images/cat-1.jpg"},
+                   innerText:"cat"}
+                  ]
+            }
+          ]
+       ]
+      }
